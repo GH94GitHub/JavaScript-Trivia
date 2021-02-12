04c84ab63d4557b4acc93cee00910fd57385a8a0
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-02-12 11:32:07 -0600

    Added title field to questionObject

--- site.js
+++ site.js
@@ -1,124 +1,133 @@
+var questionBank = { // 10 questions, each has {title, text, correctAnswer, multipleChoice{1-4}}
+'1': {
+    title: 'Question #1',
+    text: '1. Inside which HTML element do we put the JavaScript?',
+    getCorrectAnswer: function() { return this.multipleChoice[3] },
+    multipleChoice: {
+        '1': '<javascript>',
+        '2': '<js>',
+        '3': '<script>',
+        '4': '<scripting>'
+    }
+},
+'2': {
+    title: 'Question #2',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+},
+'3': {
+    title: 'Question #3',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+},
+'4': {
+    title: 'Question #4',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+},
+'5': {
+    title: 'Question #5',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+},
+'6': {
+    title: 'Question #6',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+},
+'7': {
+    title: 'Question #7',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+},
+'8': {
+    title: 'Question #8',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+},
+'9': {
+    title: 'Question #9',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+},
+'10': {
+    title: 'Question #10',
+    text: '',
+    getCorrectAnswer: function() { return },
+    multipleChoice: {
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': ''
+    }
+}
+};
+
+// ViewModel
+var ViewModel = {
+    done: ko.observable(false),
+    question: {
+        title: ko.observable(questionBank[1].title),
+        text: ko.observable(questionBank[1].text),
+        multipleChoice: {
+            a1: ko.observable(questionBank[1].multipleChoice[1]),
+            a2: ko.observable(questionBank[1].multipleChoice[2]),
+            a3: ko.observable(questionBank[1].multipleChoice[3]),
+            a4: ko.observable(questionBank[1].multipleChoice[4])
+        }
+
+        // Holds question information
+    }
+};
 window.addEventListener("load", function() {
-    var questionBank = { // 10 questions, each has {text, correctAnswer, multipleChoice{1-4}}
-        '1': {
-            text: 'Question 1',
-            correctAnswer: 'Answer1',
-            multipleChoice: {
-                '1': 'MC 1',
-                '2': 'MC 2',
-                '3': 'MC 3',
-                '4': 'MC 4'
-            }
-        },
-        '2': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        },
-        '3': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        },
-        '4': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        },
-        '5': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        },
-        '6': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        },
-        '7': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        },
-        '8': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        },
-        '9': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        },
-        '10': {
-            text: '',
-            correctAnswer: '',
-            multipleChoice: {
-                '1': '',
-                '2': '',
-                '3': '',
-                '4': ''
-            }
-        }
-    };
-
-    // ViewModel and setup
-    var ViewModel = {
-        done: ko.observable(false),
-        question: {
-            title: ko.observable('Question 1'),
-            text: ko.observable(questionBank[1].text),
-            multipleChoice: {
-                a1: ko.observable(questionBank[1].multipleChoice[1]),
-                a2: ko.observable(questionBank[1].multipleChoice[2]),
-                a3: ko.observable(questionBank[1].multipleChoice[3]),
-                a4: ko.observable(questionBank[1].multipleChoice[4])
-            }
-
-            // Holds question information
-        }
-    };
-
     ko.applyBindings(ViewModel);
 
     /* Event Listeners */
