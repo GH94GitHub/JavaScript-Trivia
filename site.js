cf2a22b90b8ad357fca4de1e5815b9eb88268656
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-02-12 15:56:20 -0600

    Added Everything Before Grading

--- site.js
+++ site.js
@@ -1,7 +1,10 @@
-var questionBank = { // 10 questions, each has {title, text, correctAnswer, multipleChoice{1-4}}
+var currentQuestion = 1;
+
+// 10 questions, each has {title, text, correctAnswer, multipleChoice{1-4}}
+var questionBank = { 
 '1': {
     title: 'Question #1',
-    text: '1. Inside which HTML element do we put the JavaScript?',
+    text: 'Inside which HTML element do we put the JavaScript?',
     getCorrectAnswer: function() { return this.multipleChoice[3] },
     multipleChoice: {
         '1': '<javascript>',
@@ -12,7 +15,7 @@ var questionBank = { // 10 questions, each has {title, text, correctAnswer, mult
 },
 '2': {
     title: 'Question #2',
-    text: '',
+    text: 'q2',
     getCorrectAnswer: function() { return },
     multipleChoice: {
         '1': '',
@@ -23,7 +26,7 @@ var questionBank = { // 10 questions, each has {title, text, correctAnswer, mult
 },
 '3': {
     title: 'Question #3',
-    text: '',
+    text: 'q3',
     getCorrectAnswer: function() { return },
     multipleChoice: {
         '1': '',
@@ -34,7 +37,7 @@ var questionBank = { // 10 questions, each has {title, text, correctAnswer, mult
 },
 '4': {
     title: 'Question #4',
-    text: '',
+    text: 'q4',
     getCorrectAnswer: function() { return },
     multipleChoice: {
         '1': '',
@@ -111,6 +114,10 @@ var questionBank = { // 10 questions, each has {title, text, correctAnswer, mult
 }
 };
 
+var storedAnswers = {
+
+};
+
 // ViewModel
 var ViewModel = {
     done: ko.observable(false),
@@ -127,41 +134,94 @@ var ViewModel = {
         // Holds question information
     }
 };
+
+// Functions
+    /**
+     * Updates the question
+     * Note: Make sure to change "currentQuestion" to desired question to be output before calling this function
+     */
+     function updateQuestion() {
+        var questionTitle = ViewModel.question.title;
+        var questionText = ViewModel.question.text;
+        var questionMultipleChoice = ViewModel.question.multipleChoice;
+
+        questionTitle(questionBank[currentQuestion].title);
+        questionText(questionBank[currentQuestion].text);
+        questionMultipleChoice.a1(questionBank[currentQuestion].multipleChoice[1]);
+        questionMultipleChoice.a2(questionBank[currentQuestion].multipleChoice[2]);
+        questionMultipleChoice.a3(questionBank[currentQuestion].multipleChoice[3]);
+        questionMultipleChoice.a4(questionBank[currentQuestion].multipleChoice[4]);
+
+        //re-check the stored answer
+        if (storedAnswers[currentQuestion]) {
+            $.each($('input[name=question]'), function() {
+                if ($(this).next().text() === storedAnswers[currentQuestion].answer) {
+                    $(this).prop('checked', true);
+                }
+            });
+        }
+        else {
+            // uncheck checked radio if there is no saved answer
+            $('input[name=question]:checked').prop('checked', false);
+        }  
+
+    }
+
+    /**
+    * If a radio button is checked store it's value and change background of quickNav item to let the user know it's completed
+    * Note: Call this before you change the "currentQuestion"
+    */
+    function storeAnswer() {
+        if ($('input[name=question]:checked')[0]) {
+            //store answer 
+            storedAnswers[currentQuestion] = {
+                answer: $('input[name=question]:checked').next().text()
+            }
+            //update quick nav background
+            $('#quickNav' + currentQuestion).css('background-color', 'rgba(0, 0, 0, .5)');
+        }
+    }
+
 window.addEventListener("load", function() {
     ko.applyBindings(ViewModel);
 
     /* Event Listeners */
-    $('#left_in_div div').bind("click", function() {
-        alert('This jumps you to question ' + $(this).html());
+    $('#left_in_div div').bind("click", function() { // Jump to desired question
+        storeAnswer();
+        currentQuestion = $(this).text();
+        updateQuestion();
     });
+
         //Buttons
-    $('#prevBtn').bind("click", function() {
-        alert('Displays the previous question.')
-    });
-
-    $('#nextBtn').bind("click", function() {
-        alert('Displays the next question.')
-    });
-
-    $('#finishBtn').bind("click", function() {
-        switchView();
-    });
-
-    $('#backBtn').bind("click", function() {
-        switchView();        
-    });
-
-    /* Switches to show result summary, or question */ 
-        function switchView() {
-        var q =$('#question');
-        var results = $('#sum_div');
-
-        if (q.is(':visible')) {
-            q.css('display', 'none');
-            results.css('display', 'block');
-        } else {
-            results.css('display', 'none');
-            q.css('display', 'block');
+    $('#prevBtn').bind("click", function() { // Previous Button
+        if (currentQuestion > 1) {
+            storeAnswer();
+            currentQuestion--;
+            updateQuestion();
         }
-    }
+        else {
+            // If user is on question 1 do something
+        }
+    });
+
+    $('#nextBtn').bind("click", function() { // Next Button
+        if (currentQuestion < 10) {
+            storeAnswer();           
+            currentQuestion++;
+            updateQuestion();
+        }
+        else {
+            // If user are on question 10 do something
+        }
+        
+    });
+
+    $('#finishBtn').bind("click", function() { // Turn-in-Paper Button
+        ViewModel.done(true);
+    });
+
+    $('#backBtn').bind("click", function() { // Back to questions button //? Should we make this a reset trivia button?
+        ViewModel.done(false);      
+    });
+
 });
