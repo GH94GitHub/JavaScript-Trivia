c04b95767d2be9ca4a90d4b18a82ac500b4077fe
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2024-05-14 11:33:28 -0500

    Changed JS Questions

--- site.js
+++ site.js
@@ -10,117 +10,137 @@
 var currentQuestion = 1;
 
 // 10 questions, each has {title, text, correctAnswer, multipleChoice{1-4}}
-var questionBank = { 
-'1': {
-    title: 'Question #1',
-    text: 'Inside which HTML element do we put the JavaScript?',
-    getCorrectAnswer: function() { return this.multipleChoice[3] },
+var questionBank = {
+  1: {
+    title: "Question #1",
+    text: "Inside which HTML element do we put JavaScript code?",
+    getCorrectAnswer: function () {
+      return this.multipleChoice[3];
+    },
     multipleChoice: {
-        '1': '<javascript>',
-        '2': '<js>',
-        '3': '<script>',
-        '4': '<scripting>'
-    }
-},
-'2': {
-    title: 'Question #2',
-    text: 'Which of the following are capabilities of functions in JavaScript?',
-    getCorrectAnswer: function() { return this.multipleChoice[3] },
+      1: "<javascript>",
+      2: "<js>",
+      3: "<script>",
+      4: "<scripting>",
+    },
+  },
+  2: {
+    title: "Question #2",
+    text: "What is the output of typeof null in JavaScript?",
+    getCorrectAnswer: function () {
+      return this.multipleChoice[3];
+    },
     multipleChoice: {
-        '1': 'Return a value',
-        '2': 'Accept parameters and Return a value',
-        '3': 'Accept parameters',
-        '4': 'None of the above'
-    }
-},
-'3': {
-    title: 'Question #3',
-    text: 'Which of the following attribute can hold the JavaScript version?',
-    getCorrectAnswer: function() { return this.multipleChoice[1] },
+      1: '"string"',
+      2: '"undefined"',
+      3: '"object"',
+      4: '"null"',
+    },
+  },
+  3: {
+    title: "Question #3",
+    text: "Which attribute holds the JavaScript version?",
+    getCorrectAnswer: function () {
+      return this.multipleChoice[1];
+    },
     multipleChoice: {
-        '1': 'LANGUAGE',
-        '2': 'SCRIPT',
-        '3': 'VERSION',
-        '4': 'None of the above'
-    }
-},
-'4': {
-    title: 'Question #4',
-    text: 'What is the correct syntax for referring to an external script called " abc.js"?',
-    getCorrectAnswer: function() { return this.multipleChoice[3] },
+      1: "LANGUAGE",
+      2: "SCRIPT",
+      3: "VERSION",
+      4: "None of the above",
+    },
+  },
+  4: {
+    title: "Question #4",
+    text: 'What is the correct syntax for embedding js code from file "abc.js" inside html code?',
+    getCorrectAnswer: function () {
+      return this.multipleChoice[3];
+    },
     multipleChoice: {
-        '1': '<script href=" abc.js">',
-        '2': '<script name=" abc.js">',
-        '3': '<script src=" abc.js">',
-        '4': 'None of the above'
-    }
-},
-'5': {
-    title: 'Question #5',
-    text: 'Which of the following best describes JavaScript?',
-    getCorrectAnswer: function() { return this.multipleChoice[4] },
+      1: '<script href="abc.js">',
+      2: '<script name="abc.js">',
+      3: '<script src="abc.js">',
+      4: "None of the above",
+    },
+  },
+  5: {
+    title: "Question #5",
+    text: "Which function is used to convert a string to an integer in JavaScript?",
+    getCorrectAnswer: function () {
+      return this.multipleChoice[4];
+    },
     multipleChoice: {
-        '1': 'a low-level programming language.',
-        '2': 'a scripting language pre-compiled in the browser.',
-        '3': 'a compiled scripting language.',
-        '4': 'an object-oriented scripting language.'
-    }
-},
-'6': {
-    title: 'Question #6',
-    text: 'Which of the following is not considered a JavaScript operator?',
-    getCorrectAnswer: function() { return this.multipleChoice[2] },
+      1: "intConvert()",
+      2: "stringToInt()",
+      3: "toInteger()",
+      4: "parseInt()",
+    },
+  },
+  6: {
+    title: "Question #6",
+    text: "Which of the following is NOT considered a JavaScript operator?",
+    getCorrectAnswer: function () {
+      return this.multipleChoice[2];
+    },
     multipleChoice: {
-        '1': 'new',
-        '2': 'this',
-        '3': 'delete',
-        '4': 'typeof'
-    }
-},
-'7': {
-    title: 'Question #7',
-    text: 'What is the correct JavaScript syntax to write "Hello World"?',
-    getCorrectAnswer: function() { return this.multipleChoice[3] },
+      1: "new",
+      2: "this",
+      3: "delete",
+      4: "typeof",
+    },
+  },
+  7: {
+    title: "Question #7",
+    text: 'What is the correct JavaScript method to write "Hello World" to the DOM?',
+    getCorrectAnswer: function () {
+      return this.multipleChoice[3];
+    },
     multipleChoice: {
-        '1': 'System.out.println("Hello World")',
-        '2': 'println ("Hello World")',
-        '3': 'document.write("Hello World")',
-        '4': 'response.write("Hello World")'
-    }
-},
-'8': {
-    title: 'Question #8',
-    text: 'JavaScript is interpreted by _________',
-    getCorrectAnswer: function() { return this.multipleChoice[1] },
+      1: 'System.out.println("Hello World")',
+      2: 'println("Hello World")',
+      3: 'document.write("Hello World")',
+      4: 'response.write("Hello World")',
+    },
+  },
+  8: {
+    title: "Question #8",
+    text: "JavaScript is interpreted by _________",
+    getCorrectAnswer: function () {
+      return this.multipleChoice[1];
+    },
     multipleChoice: {
-        '1': 'Client',
-        '2': 'Server',
-        '3': 'Object',
-        '4': 'None of the above'
-    }
-},
-'9': {
-    title: 'Question #9',
-    text: 'To automatically open the console when a JavaScript error occurs which of the following is added to prefs.js?',
-    getCorrectAnswer: function() { return this.multipleChoice[4] },
+      1: "Client",
+      2: "Server",
+      3: "Object",
+      4: "None of the above",
+    },
+  },
+  9: {
+    title: "Question #9",
+    text: 'What is the result of 10 + "5" in JavaScript?',
+    getCorrectAnswer: function () {
+      return this.multipleChoice[2];
+    },
     multipleChoice: {
-        '1': 'user_pref(" javascript.console.open_on_error", false);',
-        '2': 'user_pref("javascript.console.open_error ", true);',
-        '3': 'user_pref("javascript.console.open_error ", false);',
-        '4': 'user_pref("javascript.console.open_on_error", true);'
-    }
-},
-'10': {
-    title: 'Question #10',
-    text: 'Which is the correct way to write a JavaScript array?',
-    getCorrectAnswer: function() { return this.multipleChoice[3] },
+      1: "15",
+      2: '"105"',
+      3: "Error",
+      4: "105",
+    },
+  },
+  10: {
+    title: "Question #10",
+    text: "Which of the following is NOT a JavaScript data type?",
+    getCorrectAnswer: function () {
+      return this.multipleChoice[4];
+    },
     multipleChoice: {
-        '1': 'var txt = new Array(1:"tim",2:"kim",3:"jim")',
-        '2': 'var txt = new Array:1=("tim")2=("kim")3=("jim")',
-        '3': 'var txt = new Array("tim","kim","jim")',
-        '4': 'var txt = new Array="tim","kim","jim"'
-    }
-}
+      1: "string",
+      2: "boolean",
+      3: "array",
+      4: "float",
+    },
+  },
 };
 
 var storedAnswers = {
